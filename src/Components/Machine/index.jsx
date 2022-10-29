d6e3768170fe0a742361aae30f9e264b09f94b50
fhashimoto <felipe.hashi@gmail.com>
2022-10-29 13:30:35 -0400

    adptando a estrutura

--- src/Components/Machine/index.jsx
+++ src/Components/Machine/index.jsx
@@ -1,61 +1,71 @@
-import "./index.css"
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import wordsInMachine from "../../assets/words";
+import { randomNumber } from "../../utils/randomNumber";
 import { Emojis } from "../Emojis";
+import "./index.css";
 
-export function Machine(props){
+function playText(word) {
+  const voices = window.speechSynthesis.getVoices();
+  let utterance = new SpeechSynthesisUtterance(word);
+  const enVoices = voices.filter((el) => el.lang.includes("en"));
+  utterance.voice = enVoices[0];
+  utterance.lang = enVoices[0].lang;
+  speechSynthesis.speak(utterance);
+}
 
-    const [word, setWord] = useState("")
+export function Machine() {
+  const [word, setWord] = useState(null);
 
-    const generateWord = (event) => {
-        event.preventDefault()
-        setWord(props.wordMachine)
+  speechSynthesis.addEventListener("onvoiceschanged", playText);
+
+  const generateWord = () => {
+    const index = randomNumber({ range: wordsInMachine.length });
+    if (index >= 0 && index < wordsInMachine.length) {
+      setWord(wordsInMachine[index]);
+    } else {
+      setWord(null);
     }
+  };
 
-    function playText(word){
-        const voices = window.speechSynthesis.getVoices()
-        let utterance = new SpeechSynthesisUtterance(word)
-        utterance.voice = voices[5]
-        utterance.lang = voices[5].lang
-        speechSynthesis.speak(utterance)
+  const playAudio = () => {
+    if (word?.value) {
+      playText(word.value);
     }
+  };
 
-    speechSynthesis.addEventListener('onvoiceschanged', playText)
-
-    const playAudio = () => {
-        playText(word)
-    }
-
-    
-    return(
-        <div>
-            <div className="machine">
-                <img className="machine_gif" src="https://cdn.discordapp.com/attachments/1016745958727491615/1030525413170106529/words_machine_gif.gif"
-                alt="Words Machine Animation"/>
-                <div className="ejector_machine">
-                    <button className="button_pressme" onClick={generateWord}>
-                        PRESS ME
-                    </button>
-                </div>
-                <div className="word">
-                    <p><strong>{word}</strong></p>
-                </div>
-                
-                {word !== "" &&
-                    <>
-                    <button id="audio" className="button_sound" onClick={playAudio}></button>
-                    <div className="question">
-                        <p>Click on the corresponding image:</p>
-                    </div> 
-                    <Emojis
-                        wordMachine = {props.wordMachine}
-                        emojiMachine = {props.emojiMachine}
-                        emojisDisplay = {props.emojisDisplay}
-                        indexGenerated = {props.indexGenerated}
-                    />
-                    </>
-                }
-            </div>
-
+  return (
+    <div>
+      <div className="machine">
+        <img
+          className="machine_gif"
+          src="https://cdn.discordapp.com/attachments/1016745958727491615/1030525413170106529/words_machine_gif.gif"
+          alt="Words Machine Animation"
+        />
+        <div className="ejector_machine">
+          <button className="button_pressme" onClick={generateWord}>
+            <span>{!word ? "PRESS ME" : "RESTART"}</span>
+          </button>
         </div>
-    )
+        <div className="word">
+          <p>
+            <strong>{word?.value}</strong>
+          </p>
+        </div>
+
+        {word ? (
+          <>
+            <button
+              id="audio"
+              className="button_sound"
+              onClick={playAudio}
+            ></button>
+            <div className="question">
+              <p>Click on the corresponding image:</p>
+            </div>
+            <Emojis word={word} generateWord={generateWord} />
+          </>
+        ) : null}
+      </div>
+    </div>
+  );
 }
