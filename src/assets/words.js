c330357b1a96502cd9e7fa1874bc886c2c2fbc7e
fhashimoto <felipe.hashi@gmail.com>
2022-10-29 13:27:45 -0400

    json com os dados mockados

---
+++ src/assets/words.js
@@ -0,0 +1,134 @@
+const wordsInMachine = [
+  {
+    key: "apple",
+    value: "apple",
+    img: "https://emojipedia-us.s3.amazonaws.com/source/skype/289/red-apple_1f34e.png",
+    funFact: "The average apple has 10 seeds.",
+  },
+  {
+    key: "banana",
+    value: "banana",
+    img: "https://emojipedia-us.s3.amazonaws.com/source/skype/289/banana_1f34c.png",
+    funFact: 'A cluster of bananas is called a "hand".',
+  },
+  {
+    key: "grapes",
+    value: "grapes",
+    img: "https://emojipedia-us.s3.amazonaws.com/source/skype/289/grapes_1f347.png",
+    funFact: "Grapes are botanically classed as berries.",
+  },
+  {
+    key: "strawberry",
+    value: "strawberry",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/joypixels/340/strawberry_1f353.png",
+    funFact:
+      "Strawberries are the only fruit that wear their seeds on the outside.",
+  },
+  {
+    key: "watermelon",
+    value: "watermelon",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/joypixels/340/watermelon_1f349.png",
+    funFact: "A watermelon is approximately 92% water.",
+  },
+  {
+    key: "dog",
+    value: "dog",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/whatsapp/326/dog-face_1f436.png",
+    funFact: "Dogs can smell thousands of times better than humans.",
+  },
+  {
+    key: "cat",
+    value: "cat",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/whatsapp/326/cat-face_1f431.png",
+    funFact: "Cats can jump up to six times their length.",
+  },
+  {
+    key: "monkey",
+    value: "monkey",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/whatsapp/326/monkey-face_1f435.png",
+    funFact: "Adult monkeys can live between 10 and 50 years.",
+  },
+  {
+    key: "lion",
+    value: "lion",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/whatsapp/326/lion_1f981.png",
+    funFact: "Lions usually live in groups of 10 or 15 animals called prides.",
+  },
+  {
+    key: "pig",
+    value: "pig",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/whatsapp/326/pig-face_1f437.png",
+    funFact: "Pigs have an excellent sense of direction.",
+  },
+  {
+    key: "fish",
+    value: "fish",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/346/fish_1f41f.png",
+    funFact: "Fish have fins that help them to swim.",
+  },
+  {
+    key: "panda",
+    value: "panda",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/whatsapp/326/panda_1f43c.png",
+    funFact: "Giant pandas are good swimmers and excellent tree climbers.",
+  },
+  {
+    key: "bird",
+    value: "bird",
+    img: "https://emojipedia-us.s3.amazonaws.com/source/skype/289/bird_1f426.png",
+    funFact: "A bird's eye takes up about 50 percent of its head.",
+  },
+  {
+    key: "duck",
+    value: "duck",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/346/duck_1f986.png",
+    funFact: "Ducks can sleep with one eye open to watch for predators.",
+  },
+  {
+    key: "giraffe",
+    value: "giraffe",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/322/giraffe_1f992.png",
+    funFact: "Giraffes are the tallest mammals on Earth.",
+  },
+  {
+    key: "rabbit",
+    value: "rabbit",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/rabbit-face_1f430.png",
+    funFact: "A rabbit's teeth never stop growing.",
+  },
+  {
+    key: "tree",
+    value: "tree",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/346/deciduous-tree_1f333.png",
+    funFact:
+      "Different parts of a tree grow at different times throughout the year.",
+  },
+  {
+    key: "mushroom",
+    value: "mushroom",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/joypixels/340/mushroom_1f344.png",
+    funFact: "Some mushrooms glow in the dark.",
+  },
+  {
+    key: "sun",
+    value: "sun",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/joypixels/340/sun_2600-fe0f.png",
+    funFact: "The Sun's light reaches the Earth in eight minutes.",
+  },
+  {
+    key: "rainbow",
+    value: "rainbow",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/joypixels/340/rainbow_1f308.png",
+    funFact:
+      "A double rainbow appears when light is reflected twice in a raindrop.",
+  },
+  {
+    key: "star",
+    value: "star",
+    img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/346/star_2b50.png",
+    funFact:
+      "The smaller stars might survive for hundreds of billions of years.",
+  },
+];
+
+export default wordsInMachine;
